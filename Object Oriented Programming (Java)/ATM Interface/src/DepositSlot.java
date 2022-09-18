17ce01097df3d517aa630704d4c1b6e81ecad273
Arka Karmoker <91338507+ArkaKarmoker@users.noreply.github.com>
2022-09-18 19:50:18 +0600

    Add files via upload

---
+++ Object Oriented Programming (Java)/ATM Interface/src/DepositSlot.java
@@ -0,0 +1,8 @@
+public class DepositSlot  
+ {  
+   // indicate whether envelope was received (always returns true)  
+   // because this is only a software simulation of a real deposit slot  
+   public boolean isEnvelopeReceived(){  
+     return true;  // deposit envelope was received  
+   }  // end method isEnvelopeReceived  
+ }  // end class DepositSlot
